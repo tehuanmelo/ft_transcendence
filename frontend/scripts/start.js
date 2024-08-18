2e05ce0b5e403719878a895213159b6582da0040
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-08-18 15:52:29 +0400

    feat(frontend): cleaned up design

--- frontend/scripts/start.js
+++ frontend/scripts/start.js
@@ -7,7 +7,18 @@ form.addEventListener('submit', (e) => {
 });
 
 // ! to remove
-const btn = document.getElementById('login-btn');
-btn.addEventListener('click', () => {
-    console.log('click');
+document.getElementById('login-btn').addEventListener('click', () => {
+    console.log('logging in...');
+});
+
+document.getElementById('42-login-btn').addEventListener('click', () => {
+    console.log('logging in with 42...');
+});
+
+document.getElementById('guest-login-btn').addEventListener('click', () => {
+    console.log('logging in as guest...');
+});
+
+document.getElementById('sign-up').addEventListener('click', () => {
+    console.log('signing up...');
 });
