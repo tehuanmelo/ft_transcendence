ba86f9626d248df6c0c9ba127399fa05662c8b31
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-08-18 20:31:04 +0400

    feat(Frontend Login): designed login form
    - removed home.html

---
+++ frontend/scripts/login-form.js
@@ -0,0 +1,16 @@
+// ! to remove
+document.getElementById('login-btn').addEventListener('click', () => {
+    console.log('logging in...');
+});
+
+document.getElementById('42-login-btn').addEventListener('click', () => {
+    console.log('logging in with 42...');
+});
+
+document.getElementById('guest-login-btn').addEventListener('click', () => {
+    console.log('logging in as guest...');
+});
+
+document.getElementById('sign-up-btn').addEventListener('click', () => {
+    console.log('signing up...');
+});
