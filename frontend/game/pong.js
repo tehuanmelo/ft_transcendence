6ee326369d5166b810a4a62123ee14d34ee74793
Paula Balbino <lola.paulinha@gmail.com>
2024-08-26 22:39:17 +0400

    feat(game): button legend and key events (in progress)

--- frontend/game/pong.js
+++ frontend/game/pong.js
@@ -29,32 +29,73 @@ class Ball {
 }
 
 class Paddle {
-	constructor (paddleType)
-	{
+	constructor(paddleType) {
 		this.paddleType = paddleType;
 		this.width = canvas.width; // Canvas width in pixels
-        this.height = canvas.height; // Canvas height in pixels
+		this.height = canvas.height; // Canvas height in pixels
+		if (this.paddleType == PaddleTypes.LEFT1) {
+			this.x = 2;
+			this.y = this.height / 2.5;
+		}
+		if (this.paddleType == PaddleTypes.RIGHT1) {
+			this.x = this.width - 2 - this.width / 50;
+			this.y = this.height / 2.5;
+		}
+		if (this.paddleType == PaddleTypes.LEFT2) {
+			this.x = this.width / 50 + 2 + 3;
+			this.y = this.height / 2.5;
+		}
+		if (this.paddleType == PaddleTypes.RIGHT2) {
+			this.x = this.width - 5 - this.width / 50 - this.width / 50;
+			this.y = this.height / 2.5;
+		}
 	}
+	up() {
+		if (this.y > 0) {
+			this.y -= 8; // pixels to move per key event
+		}
+	}
+
+	// Check if the paddle is not at the bottom of the canvas
+	down() {
+		if (this.y < this.height - this.height / 8) {
+			this.y += 10;
+		}
+	}
+
+
 	drawPaddle() {
-		ctx.fillStyle = 'white'; // Fill color
+		ctx.fillStyle = 'white'; // Fill color players 1 and 2
 		if (this.paddleType == PaddleTypes.LEFT1)
-		ctx.fillRect(2, this.height/2.5, this.width/50, this.height/8); // Draw a rectangle (x, y, width, height)
-		if (this.paddleType == PaddleTypes.LEFT2)
-			ctx.fillRect(this.width/50 + 2 + 3, this.height/2.5, this.width/50, this.height/8);
+		ctx.fillRect(this.x, this.y, this.width/50, this.height/8); // Draw a rectangle (x, y, width, height)
 		if (this.paddleType == PaddleTypes.RIGHT1)
-			ctx.fillRect(this.width - 2 - this.width/50, this.height/2.5, this.width/50, this.height/8);
+			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
+		ctx.fillStyle = '#dc3545'; // Fill color players 3 and 4
+		if (this.paddleType == PaddleTypes.LEFT2)
+			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
 		if (this.paddleType == PaddleTypes.RIGHT2)
-			ctx.fillRect(this.width - 5 - this.width/50 -this.width/50, this.height/2.5, this.width/50, this.height/8);
+			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
 	}
 }
 
 class Pong {
 	constructor(nbPlayers) {
-		this.nbPlayers = nbPlayers;
+		// Get canvas attributes
+		this.width = canvas.width; // Canvas width in pixels
+		this.height = canvas.height; // Canvas height in pixels
 
-        // Get canvas attributes
-        this.width = canvas.width; // Canvas width in pixels
-        this.height = canvas.height; // Canvas height in pixels
+		this.nbPlayers = nbPlayers;
+		this.paddle1 = new Paddle(PaddleTypes.LEFT1);
+		this.paddle2 = new Paddle(PaddleTypes.RIGHT1);
+
+		if (this.nbPlayers == 4) {
+			this.paddle3 = new Paddle(PaddleTypes.LEFT2);
+			this.paddle4 = new Paddle(PaddleTypes.RIGHT2);
+		}
+	}
+	start() {
+		setInterval(this.pongRender.bind(this), 1000 / 60); // 60 FPS (frames per second)
+		document.addEventListener('keydown', this.handleKeyboardEvent.bind(this)); // addEventListener is a system function
 	}
 	drawSquare() {
 		ctx.fillStyle = 'black'; // Fill color
@@ -72,12 +113,55 @@ class Pong {
 		ctx.lineTo(this.width / 2, this.height); //  Line final point
 		ctx.stroke(); // Apply line stile and draw
 	}
+	pongRender() {
+		this.drawSquare();
+		this.paddle1.drawPaddle();
+		this.paddle2.drawPaddle();
+		if (this.nbPlayers == 4) {
+			this.paddle3.drawPaddle();
+			this.paddle4.drawPaddle();
+		}
+	}
+
+	handleKeyboardEvent(event) {
+		// Check if the key is a letter or control key
+		if (event.key === 'q') {
+			this.paddle1.up();
+		} else if (event.key === 'a') {
+			this.paddle1.down();
+		}
+	}
+
 }
-paddle1 = new Paddle(PaddleTypes.LEFT1);
-paddle2 = new Paddle(PaddleTypes.RIGHT1);
+
+class Control {
+	constructor() {
+		this.width = canvas.width; // Canvas width in pixels
+		this.height = canvas.height; // Canvas height in pixels
+	}
+
+	drawControlText() {
+		// Set text style
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		ctx.fillStyle = 'white';
+
+		// Define the starting y position for the first line of text
+		lineHeight = fontSize * 0.8; // Space between lines
+		let startY = canvasHeight / 2 - (lineHeight * (textLines.length - 0.8)) / 2;
+
+		for (const line of textLines) {
+			ctx.fillText(line, canvasWidth / 2, startY);
+			startY += lineHeight;
+		}
+	}
+}
+
+/* paddle1 = new Paddle(PaddleTypes.LEFT1);
+ paddle2 = new Paddle(PaddleTypes.RIGHT1);
 paddle3 = new Paddle(PaddleTypes.LEFT2);
 paddle4 = new Paddle(PaddleTypes.RIGHT2);
-pong = new Pong(2);
+
 ball = new Ball();
 
 pong.drawSquare();
@@ -85,4 +169,7 @@ paddle1.drawPaddle();
 paddle2.drawPaddle();
 paddle3.drawPaddle();
 paddle4.drawPaddle();
-ball.drawBall();
+ball.drawBall(); */
+pong = new Pong(2);
+pong.start();
+
