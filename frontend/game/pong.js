7963fb164c9a3e26d7f9d676481a44f44e18e99d
Paula Balbino <lola.paulinha@gmail.com>
2024-08-29 21:40:17 +0400

    feat(game): fixed vertical collision + added ball moviment

--- frontend/game/pong.js
+++ frontend/game/pong.js
@@ -15,19 +15,63 @@ class Ball {
 	{
 		this.width = canvas.width;
 		this.height = canvas.height;
+		this.ballMoving = false;
+		this.speedX = 5; // Ball speed in the x direction
+		this.speedY = 5; // Ball speed in the y direction
+		this.ballX = this.width/2; // X position of the ball
+		this.ballY = this.height/2; // Y position of the ball
+		this.dx = 0; // Change in x direction
+		this.dy = 0; // Change in y direction
 	}
+
+
 
 	drawBall() {
 		ctx.fillStyle = 'white'; // Fill color
-		var ballX = this.width/2; // X position of the ball
-		var ballY = this.height/2; // Y position of the ball
-		var ballRadius = 3; // Radius of the ball
+
+		var ballRadius = this.width/100; // Radius of the ball
+
+		//this.ballMoving = false; // Track if the ball is moving
 		ctx.beginPath(); // Start a new path
-		ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2, false); // Draw a circle
+		ctx.arc(this.ballX, this.ballY, ballRadius, 0, Math.PI * 2, false); // Draw a circle
 		ctx.fillStyle = 'white'; // Fill color
 		ctx.fill(); // Fill the circle with color
 		ctx.closePath(); // Close the path
 	}
+
+	ballStartAngle() {
+		var min = -0.5;
+		var max = 0.5;
+		var angle = Math.random() * (max - min) + min;
+		if (angle < 0.15 && angle > -0.15) {
+			angle = 0.15;
+		}
+		return angle;
+	}
+
+	startBallMovement() {
+		this.dx = this.ballStartAngle() * this.speedX; // Initialize dx based on random angle
+		this.dy = this.ballStartAngle() * this.speedY; // Initialize dy based on random angle
+		this.ballMoving = true; // Set flag to true to start movement
+	}
+
+	ballMove() {
+		if (this.ballMoving) {
+			// Bounce off top and bottom walls
+			if (this.ballY + this.dy > canvas.height - Pong.REC_HEIGHT_SIZE || this.ballY + this.dy < Pong.REC_HEIGHT_SIZE) {
+				this.dy = -this.dy;
+			}
+			// Bounce off left and right walls
+			//if (this.ballX + this.dx > canvas.width || this.ballX + this.dx < 0) {
+			//	this.dx = -this.dx;
+			//}
+			// Update ball position
+			this.ballX += this.dx;
+			this.ballY += this.dy;
+		}
+	}
+
+
 }
 
 class Paddle {
@@ -53,15 +97,20 @@ class Paddle {
 		}
 	}
 	up() {
-		if (this.y > Pong.REC_HEIGHT_SIZE + 5) {
+		if (this.y - Pong.PADDLE_SPEED - Pong.REC_HEIGHT_SIZE < 0) {
+			this.y = Pong.REC_HEIGHT_SIZE; // pixels to move per key event
+		}
+		else if (this.y > Pong.REC_HEIGHT_SIZE) {
 			this.y -= Pong.PADDLE_SPEED; // pixels to move per key event
 		}
 	}
 
 	// Check if the paddle is not at the bottom of the canvas
 	down() {
-		if (this.y < this.height - Pong.REC_HEIGHT_SIZE - this.height/8 - 5) {
-
+		if (this.y + Pong.REC_HEIGHT_SIZE + this.height/8 + 5 > this.height) {
+			this.y = this.height - Pong.REC_HEIGHT_SIZE - this.height/8 - 5;
+		}
+		else if (this.y < this.height - Pong.REC_HEIGHT_SIZE - this.height/8 - 5) {
 			this.y += Pong.PADDLE_SPEED;
 		}
 	}
@@ -70,7 +119,12 @@ class Paddle {
 	drawPaddle() {
 		ctx.fillStyle = 'white'; // Fill color players 1 and 2
 		if (this.paddleType == PaddleTypes.LEFT1)
-		ctx.fillRect(this.x, this.y, this.width/50, this.height/8); // Draw a rectangle (x, y, width, height)
+		{
+			console.log(this.y);
+			ctx.fillRect(this.x, this.y, this.width/50, this.height/8); // Draw a rectangle (x, y, width, height)
+		}
+
+
 		if (this.paddleType == PaddleTypes.RIGHT1)
 			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
 		ctx.fillStyle = '#dc3545'; // Fill color players 3 and 4
@@ -88,11 +142,11 @@ class Pong {
 		// Get canvas attributes
 		this.width = canvas.width; // Canvas width in pixels
 		this.height = canvas.height; // Canvas height in pixels
-
 		this.nbPlayers = nbPlayers;
 		this.paddle1 = new Paddle(PaddleTypes.LEFT1);
 		this.paddle2 = new Paddle(PaddleTypes.RIGHT1);
 		this.score = new Score();
+		this.ball = new Ball();
 
 		if (this.nbPlayers == 4) {
 			this.paddle3 = new Paddle(PaddleTypes.LEFT2);
@@ -102,6 +156,11 @@ class Pong {
 	start() {
 		setInterval(this.pongRender.bind(this), 1000 / 60); // 60 FPS (frames per second)
 		document.addEventListener('keydown', this.handleKeyboardEvent.bind(this)); // addEventListener is a system function
+		document.addEventListener('keydown', (event) => {
+			if (event.key === 'Enter') {
+				this.ball.startBallMovement(); // Start ball movement on Enter key press
+			}
+		});
 	}
 	drawSquare() {
 		ctx.fillStyle = 'black'; // Fill color
@@ -110,7 +169,7 @@ class Pong {
 		// Set properties for the rectangle
 		ctx.fillStyle = 'white'; // Fill color
 		ctx.fillRect(0, 0, this.width, Pong.REC_HEIGHT_SIZE); // Draw a rectangle (x, y, width, height)
-		console.log(this.height);
+		//console.log(this.height);
 		ctx.fillRect(0, this.height - Pong.REC_HEIGHT_SIZE, this.width, Pong.REC_HEIGHT_SIZE); // Draw a rectangle (x, y, width, height)
 		ctx.strokeStyle = 'white'; // Line color
 		ctx.lineWidth = 2; // line Width
@@ -129,7 +188,6 @@ class Pong {
 		// Define the starting y position for the first line of text
 		lineHeight = fontSize * 0.8; // Space between lines
 		let startY = canvasHeight / 2 - (lineHeight * (textLines.length - 0.8)) / 2;
-
 		ctx.fillText(line, canvasWidth / 2, startY);
 
 	}
@@ -142,7 +200,8 @@ class Pong {
 			this.paddle4.drawPaddle();
 		}
 		this.score.drawScore();
-
+		this.ball.drawBall(); // Draw the ball
+		this.ball.ballMove(); // Move the ball
 	}
 
 	handleKeyboardEvent(event) {
@@ -172,7 +231,9 @@ class Pong {
 			}
 		}
 	}
+	// paddleCollision() { } // TO DO: Implement paddle collision
 }
+
 
 class Score {
 	constructor() {
@@ -204,16 +265,17 @@ class Score {
 
 
 
-
-
-
-
-
-
-
-
-
-
 pong = new Pong(4);
 pong.start();
+//requestAnimationFrame(pong.pongRender.bind(pong));
 
+
+// Game over: pop up message?
+// Add 2 balls?
+// Add sound effects?
+// Custom score?
+// Add different ball speeds?
+// Add different paddle speeds? paddle colors?
+// Add different paddle sizes?
+// Add different ball sizes?
+// Add different backgrounds/themes?
