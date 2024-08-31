eaa3940a62dedb9a5f44b76df550aab6fe499698
Paula Balbino <lola.paulinha@gmail.com>
2024-08-31 15:30:03 +0400

    feat(game): score implemented, paddel collision, game over pop-up

--- frontend/game/pong.js
+++ frontend/game/pong.js
@@ -11,29 +11,31 @@ const PaddleTypes = Object.freeze({
 });
 
 class Ball {
-	constructor ()
-	{
+	constructor (score, nbPlayers, paddle1, paddle2, paddle3, paddle4) {
 		this.width = canvas.width;
 		this.height = canvas.height;
 		this.ballMoving = false;
-		this.speedX = 5; // Ball speed in the x direction
-		this.speedY = 5; // Ball speed in the y direction
+		this.speedX = 15; // Ball speed in the x direction
+		this.speedY = 15; // Ball speed in the y direction
 		this.ballX = this.width/2; // X position of the ball
 		this.ballY = this.height/2; // Y position of the ball
 		this.dx = 0; // Change in x direction
 		this.dy = 0; // Change in y direction
+		this.paddle1 = paddle1;
+		this.paddle2 = paddle2;
+		this.paddle3 = paddle3;
+		this.paddle4 = paddle4;
+		this.nbPlayers = nbPlayers;
+		this.ballRadius = this.width/100;
+		this.score = score;
 	}
-
-
 
 	drawBall() {
 		ctx.fillStyle = 'white'; // Fill color
 
-		var ballRadius = this.width/100; // Radius of the ball
-
 		//this.ballMoving = false; // Track if the ball is moving
 		ctx.beginPath(); // Start a new path
-		ctx.arc(this.ballX, this.ballY, ballRadius, 0, Math.PI * 2, false); // Draw a circle
+		ctx.arc(this.ballX, this.ballY, this.ballRadius, 0, Math.PI * 2, false); // Draw a circle
 		ctx.fillStyle = 'white'; // Fill color
 		ctx.fill(); // Fill the circle with color
 		ctx.closePath(); // Close the path
@@ -49,6 +51,11 @@ class Ball {
 		return angle;
 	}
 
+	ballResetPosition() {
+		this.ballX = this.width/2;
+		this.ballY = this.height/2;
+	}
+
 	startBallMovement() {
 		this.dx = this.ballStartAngle() * this.speedX; // Initialize dx based on random angle
 		this.dy = this.ballStartAngle() * this.speedY; // Initialize dy based on random angle
@@ -57,20 +64,69 @@ class Ball {
 
 	ballMove() {
 		if (this.ballMoving) {
-			// Bounce off top and bottom walls
-			if (this.ballY + this.dy > canvas.height - Pong.REC_HEIGHT_SIZE || this.ballY + this.dy < Pong.REC_HEIGHT_SIZE) {
+			// top and bottom walls
+			if (this.ballY + this.dy + this.ballRadius > canvas.height - Pong.REC_HEIGHT_SIZE || this.ballY + this.dy - this.ballRadius < Pong.REC_HEIGHT_SIZE) {
 				this.dy = -this.dy;
 			}
-			// Bounce off left and right walls
+			// left and right walls TO DO: remove if we dont implement this mode in the future
 			//if (this.ballX + this.dx > canvas.width || this.ballX + this.dx < 0) {
 			//	this.dx = -this.dx;
 			//}
-			// Update ball position
+			console.log(this.ballX +" "+ this.dx  +" "+  this.ballRadius)
+			// left paddle collision
+			if (this.ballX + this.dx - this.ballRadius < this.paddle1.x + this.paddle1.paddleWidth && this.ballY > this.paddle1.y && this.ballY < this.paddle1.y + this.paddle1.paddleHeight) {
+				console.log("inverte");
+				this.dx = -this.dx;
+			}
+			// right paddle collision
+			if (this.ballX + this.dx + this.ballRadius > this.paddle2.x && this.ballY > this.paddle2.y && this.ballY < this.paddle2.y + this.paddle2.paddleHeight) {
+				this.dx = -this.dx;
+			}
+
+			if (this.nbPlayers == 4) {
+				// left paddle collision (player 3)
+				if (this.ballX + this.dx - this.ballRadius < this.paddle3.x + this.paddle3.paddleWidth && this.ballY > this.paddle3.y && this.ballY < this.paddle3.y + this.paddle3.paddleHeight) {
+					this.dx = -this.dx;
+				}
+				// right paddle collision (player 4)
+				if (this.ballX + this.dx + this.ballRadius > this.paddle4.x && this.ballY > this.paddle4.y && this.ballY < this.paddle4.y + this.paddle4.paddleHeight) {
+					this.dx = -this.dx;
+				}
+			}
+
 			this.ballX += this.dx;
 			this.ballY += this.dy;
+
+			this.updateScore();
+
 		}
+
+
 	}
 
+	//this.ballX = this.width/2;
+	//this.ballY = this.height/2;
+	//this.ballMoving = true;
+
+	updateScore() {
+		if (this.ballX < 0) {
+			this.score.incrementScoreR();
+			this.ballMoving = false;
+			if (this.score.checkGameOver() === false) {
+				this.ballResetPosition();
+				this.startBallMovement();
+			}
+
+		}
+		if (this.ballX > canvas.width) {
+			this.score.incrementScoreL();
+			this.ballMoving = false;
+			if (this.score.checkGameOver() === false) {
+				this.ballResetPosition();
+				this.startBallMovement();
+			}
+		}
+	}
 
 }
 
@@ -79,20 +135,22 @@ class Paddle {
 		this.paddleType = paddleType;
 		this.width = canvas.width; // Canvas width in pixels
 		this.height = canvas.height; // Canvas height in pixels
+		this.paddleWidth = this.width/50;
+		this.paddleHeight = this.height/8;
 		if (this.paddleType == PaddleTypes.LEFT1) {
 			this.x = 2;
 			this.y = this.height / 2.5;
 		}
 		if (this.paddleType == PaddleTypes.RIGHT1) {
-			this.x = this.width - 2 - this.width / 50;
+			this.x = this.width - 2 - this.paddleWidth;
 			this.y = this.height / 2.5;
 		}
 		if (this.paddleType == PaddleTypes.LEFT2) {
-			this.x = this.width / 50 + 2 + 3;
+			this.x = this.paddleWidth + 2 + 3;
 			this.y = this.height / 2.5;
 		}
 		if (this.paddleType == PaddleTypes.RIGHT2) {
-			this.x = this.width - 5 - this.width / 50 - this.width / 50;
+			this.x = this.width - 5 - this.paddleWidth - this.paddleWidth;
 			this.y = this.height / 2.5;
 		}
 	}
@@ -107,31 +165,30 @@ class Paddle {
 
 	// Check if the paddle is not at the bottom of the canvas
 	down() {
-		if (this.y + Pong.REC_HEIGHT_SIZE + this.height/8 + 5 > this.height) {
-			this.y = this.height - Pong.REC_HEIGHT_SIZE - this.height/8 - 5;
+		if (this.y + Pong.REC_HEIGHT_SIZE + this.paddleHeight + 5 > this.height) {
+			this.y = this.height - Pong.REC_HEIGHT_SIZE - this.paddleHeight - 5;
 		}
-		else if (this.y < this.height - Pong.REC_HEIGHT_SIZE - this.height/8 - 5) {
+		else if (this.y < this.height - Pong.REC_HEIGHT_SIZE - this.paddleHeight - 5) {
 			this.y += Pong.PADDLE_SPEED;
 		}
 	}
 
+	// TO DO: Implement paddle collision when the ball hits the paddle on the top or bottom of the paddle
 
 	drawPaddle() {
 		ctx.fillStyle = 'white'; // Fill color players 1 and 2
 		if (this.paddleType == PaddleTypes.LEFT1)
 		{
-			console.log(this.y);
-			ctx.fillRect(this.x, this.y, this.width/50, this.height/8); // Draw a rectangle (x, y, width, height)
+			ctx.fillRect(this.x, this.y, this.paddleWidth, this.paddleHeight); // Draw a rectangle (x, y, width, height)
 		}
 
-
 		if (this.paddleType == PaddleTypes.RIGHT1)
-			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
+			ctx.fillRect(this.x, this.y, this.paddleWidth, this.paddleHeight);
 		ctx.fillStyle = '#dc3545'; // Fill color players 3 and 4
 		if (this.paddleType == PaddleTypes.LEFT2)
-			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
+			ctx.fillRect(this.x, this.y, this.paddleWidth, this.paddleHeight);
 		if (this.paddleType == PaddleTypes.RIGHT2)
-			ctx.fillRect(this.x, this.y, this.width/50, this.height/8);
+			ctx.fillRect(this.x, this.y, this.paddleWidth, this.paddleHeight);
 	}
 }
 
@@ -146,15 +203,22 @@ class Pong {
 		this.paddle1 = new Paddle(PaddleTypes.LEFT1);
 		this.paddle2 = new Paddle(PaddleTypes.RIGHT1);
 		this.score = new Score();
-		this.ball = new Ball();
 
 		if (this.nbPlayers == 4) {
 			this.paddle3 = new Paddle(PaddleTypes.LEFT2);
 			this.paddle4 = new Paddle(PaddleTypes.RIGHT2);
+			this.ball = new Ball(this.score, 4, this.paddle1, this.paddle2, this.paddle3, this.paddle4);
 		}
+		else
+		{
+			this.ball = new Ball(this.score, 2, this.paddle1, this.paddle2);
+		}
+		this.intervalId =0;
 	}
 	start() {
-		setInterval(this.pongRender.bind(this), 1000 / 60); // 60 FPS (frames per second)
+		this.score.resetScore();
+		this.ball.ballResetPosition();
+		this.intervalId =setInterval(this.pongRender.bind(this), 1000 / 60); // 60 FPS (frames per second)
 		document.addEventListener('keydown', this.handleKeyboardEvent.bind(this)); // addEventListener is a system function
 		document.addEventListener('keydown', (event) => {
 			if (event.key === 'Enter') {
@@ -162,6 +226,12 @@ class Pong {
 			}
 		});
 	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		document.removeEventListener('keydown', this.handleKeyboardEvent.bind(this));
+	}
+
 	drawSquare() {
 		ctx.fillStyle = 'black'; // Fill color
 		ctx.fillRect(0, 0, this.width, this.height); // Draw a rectangle (x, y, width, height)
@@ -191,7 +261,21 @@ class Pong {
 		ctx.fillText(line, canvasWidth / 2, startY);
 
 	}
-	pongRender() {
+
+	displayWinner() {
+		var element = document.getElementById('winner');
+
+		if (this.score.scoreL == this.score.finalScore) {
+			element.innerText = "Left Player wins!";
+		}
+		else {
+			element.innerText = "Right Player wins!";
+		}
+		var myModal = new bootstrap.Modal(document.getElementById('winnerpopup'));
+			myModal.show(); // Show the modal
+	}
+
+	render() {
 		this.drawSquare();
 		this.paddle1.drawPaddle();
 		this.paddle2.drawPaddle();
@@ -202,6 +286,16 @@ class Pong {
 		this.score.drawScore();
 		this.ball.drawBall(); // Draw the ball
 		this.ball.ballMove(); // Move the ball
+	}
+
+	pongRender() {
+		this.render();
+
+		if (this.score.checkGameOver() == true) {
+			this.displayWinner();
+			this.stop();
+			this.render();
+		}
 	}
 
 	handleKeyboardEvent(event) {
@@ -231,7 +325,6 @@ class Pong {
 			}
 		}
 	}
-	// paddleCollision() { } // TO DO: Implement paddle collision
 }
 
 
@@ -239,17 +332,24 @@ class Score {
 	constructor() {
 		this.scoreL = 0;
 		this.scoreR = 0;
-		this.finalScore = 11; // TO DO: final score: allow custom score
+		this.finalScore = 3; // TO DO: final score: allow custom score
 	}
-	//if(ball.x < 0) {
-	//	this.scoreR++;
 
-	//}
-	//if(ball.x > canvas.width) {
-	//this.scoreL++;
-	//if (this.scoreL === finalScore || this.scoreR === finalScore) {
-	// Game over
-	//}
+	incrementScoreR() {
+		this.scoreR++;
+	}
+
+	incrementScoreL() {
+		this.scoreL++;
+	}
+
+	checkGameOver() {
+		if (this.scoreL === this.finalScore || this.scoreR === this.finalScore) {
+			return true;
+		}
+		return false;
+	}
+
 
 	drawScore() {
 	ctx.font = "72px Bungee Tint";
@@ -267,10 +367,13 @@ class Score {
 
 pong = new Pong(4);
 pong.start();
-//requestAnimationFrame(pong.pongRender.bind(pong));
+
+function playAgain() {
+	pong.start();
+}
 
 
-// Game over: pop up message?
+
 // Add 2 balls?
 // Add sound effects?
 // Custom score?
