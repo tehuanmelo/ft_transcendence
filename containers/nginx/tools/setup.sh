9a516dd031638fa1e0bb926b314280c248841e22
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-09-04 21:27:39 +0400

    chore: cleaned up NGINX dockerfile
    - moved stuff out of setup.sh and into dockerfile
    - changed location of ./frontend mount
    - updated conf file to now ensure proper error handling in case of
      reloads

--- containers/nginx/tools/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# install dependencies
-apk add --update --no-cache openssl curl ca-certificates nginx
-
-#! create an ssl certificate [should be signed properly???]
-mkdir /etc/nginx/certs && cd /etc/nginx/certs && \
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/certs/test.crt \
-	-keyout /etc/nginx/certs/key.key -subj "/C=US/ST=None/L=AbuDhabi/O=Certifie/CN=pong.com"
