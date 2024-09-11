62b9ab227a21be642b5da4bd3eba5b7d9cedeeb5
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-09-11 15:25:24 +0400

    feat: updated users app and implemented template files

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,22 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 CustomUser = get_user_model()
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = (
-        "username",
-        "email",
-        "is_superuser",
-    )
-
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('display_name',)}),  # Add display_name to fieldsets
-    )
+    list_display = [
+        'email',
+        'username',
+        'is_superuser',
+    ]
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'users'
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('<int:pk>/', views.UsersDetailView.as_view()),
-    path('list/', views.UsersListView.as_view()),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+    # path('<int:pk>/', views.UsersDetailView.as_view()),
+    # path('list/', views.UsersListView.as_view()),
 ]
