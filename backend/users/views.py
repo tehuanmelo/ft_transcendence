db019c4058593a837a61c950f75c350cf98d6939
tehuanmelo <tehuanmelo@gmail.com>
2024-09-20 22:56:44 +0400

    feat(jwt): generate and store JSON Web Token (JWT) as a cookie
    - Implemented functionality to generate a JWT and store it as a cookie.
    - Additional configuration is still in development, including adding a decorator to validate the token.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,15 +1,45 @@
+
+import pyotp, qrcode, os, jwt, datetime, pytz
+
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-import pyotp, qrcode
-import os
 from django.conf import settings
-
 
 from .forms import CustomUserCreationForm, UserProfileForm
 from .models import CustomUser
+from .utils import generate_key_qrcode, generate_jwt
+
+
+
+def generate_key_qrcode(user):
+    secret = pyotp.random_base32()
+    qrcode_url = pyotp.totp.TOTP(secret).provisioning_uri(name=user.username, issuer_name="PONG")
+    
+    file_path = os.path.join(settings.MEDIA_ROOT, 'qr_codes', f'{user.username}_qrcode.png')
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    qrcode.make(qrcode_url).save(file_path)
+    
+    user.google_auth_key = secret
+    user.qrcode_img.name = f'qr_codes/{user.username}_qrcode.png'
+    user.is_2fa_set = False
+    user.save()
+    
+    
+    
+def generate_jwt(user):
+    utc = pytz.UTC
+    expiration_time = datetime.datetime.now(utc) + datetime.timedelta(hours=1)
+    payload = {
+        'user_id': user.id,
+        'username': user.username,
+        'exp': expiration_time
+    }
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return token
+
 
 def reset_2fa_view(request):
     user_id = request.session['user_id']
@@ -18,23 +48,7 @@ def reset_2fa_view(request):
     return redirect("verify_otp")
     
 
-def generate_key_qrcode(user):
-    
-    secret = pyotp.random_base32()
-    qrcode_url = pyotp.totp.TOTP(secret).provisioning_uri(name=user.username, issuer_name="PONG")
-    
-    file_path = os.path.join(settings.MEDIA_ROOT, 'qr_codes', f'{user.username}_qrcode.png')
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
-    qrcode.make(qrcode_url).save(file_path)
-    
-    user.google_auth_key = secret
-    user.qrcode_img.name = f'qr_codes/{user.username}_qrcode.png'
-    user.is_2fa_set = False
-    user.save()
-
-  
-def verify_otp(request):
+def verify_otp_view(request):
     
     user_id = request.session['user_id']
     user = CustomUser.objects.get(id=user_id)
@@ -51,20 +65,22 @@ def verify_otp(request):
         totp = pyotp.TOTP(key)
         
         if totp.verify(otp):
-            # del request.session
             login(request, user)
-            # set the variable to true to not show the qrcode again
+            # set the variable to true for not showing the qrcode again
             user.is_2fa_set = True 
             user.save()
-            return redirect("home")
+            
+            token = generate_jwt(user)
+            print(token)
+            response = redirect("home")
+            response.set_cookie("jwt", token, httponly=True, secure=True)
+            return response
         else:
             obj["error"] = True
     
     return render(request, "users/two_fact_auth.html", obj)
             
-        
-     
-    
+          
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
