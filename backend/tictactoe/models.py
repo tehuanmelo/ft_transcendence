40d1c18a43b54ebd4f5d6a84a19fc522116c4948
Malik AlZubaidi <malik@leam.ae>
2024-10-21 21:44:36 +0400

    feat: init routes and model

--- backend/tictactoe/models.py
+++ backend/tictactoe/models.py
@@ -1,3 +1,14 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
+class TicTacToeGame(models.Model):
+
+	board = ArrayField(
+		ArrayField(
+			models.CharField(max_length=10, blank=True),
+			size=3,
+		),
+		size=3,
+	)
+
--- backend/tictactoe/views.py
+++ backend/tictactoe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from users.auth import jwt_login_required
+from tictactoe.models import TicTacToeGame
+
+@jwt_login_required
+def ttt(request):
+    print ("..")
+    return render(request, "ttt/ttt.html")
 
 # Create your views here.
