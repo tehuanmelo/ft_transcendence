df4bf2e0a6ae88d3fe59133d5e180d0f7f6d692d
Malik AlZubaidi <malik@leam.ae>
2024-10-24 20:56:40 +0400

    feat: update match to have enums for win/loss + override save

--- backend/users/models/match.py
+++ backend/users/models/match.py
@@ -12,7 +12,24 @@ class Match(models.Model):
     date = models.DateTimeField(
         auto_now_add=True
     )  # Automatically set the date when the match is created
-    result = models.CharField(max_length=10)  # 'win' or 'loss'
+    class MatchResult(models.TextChoices):
+        WIN = 'win', 'Win'
+        LOSS = 'loss', 'Loss'
+
+    result = models.CharField(
+        max_length=10,
+        choices=MatchResult.choices,
+        default=MatchResult.LOSS,
+    )
+
+    @property
+    def is_win(self):
+        return self.MatchResult(self.result) == MatchResult.WIN
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.user.update_stats()
+        self.opponent.update_stats()
 
     def __str__(self):
         return f"{self.user.username} vs {self.opponent.username} on {self.date}"
