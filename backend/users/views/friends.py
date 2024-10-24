62d626824c160714e6a5fb8f9f66ca5160b8b628
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-10-24 14:05:40 +0400

    chore: added comments to urls.py

--- backend/users/views/friends.py
+++ backend/users/views/friends.py
@@ -133,19 +133,3 @@ def reject_friend(request, username):
         return JsonResponse({"error_message": error_message}, status=status)
 
     return redirect("friends")
-
-
-##############
-##### Match Record Methods
-##############
-
-
-@jwt_login_required
-def record_match_result(winner, loser):
-    winner.wins += 1
-    loser.losses += 1
-    winner.save()
-    loser.save()
-
-    Match.objects.create(user=winner, opponent=loser)
-    Match.objects.create(user=loser, opponent=winner)
