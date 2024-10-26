d9fa930da161a5a4d0574ccc79ff8fb9a1cc4b6c
Malik AlZubaidi <malik@leam.ae>
2024-10-26 13:55:46 +0400

    feat: update tictactoe game model

--- backend/tictactoe/models.py
+++ backend/tictactoe/models.py
@@ -3,12 +3,18 @@ from django.db import models
 
 
 class TicTacToeGame(models.Model):
-
-	board = ArrayField(
-		ArrayField(
-			models.CharField(max_length=10, blank=True),
-			size=3,
-		),
-		size=3,
-	)
-
+    user = models.ForeignKey(
+        "users.CustomUser", on_delete=models.CASCADE, related_name="tictactoe_games"
+    )
+    opponent = models.CharField(max_length=20)
+    board = ArrayField(
+        ArrayField(
+            models.CharField(max_length=1, blank=True),
+            size=3,
+        ),
+        size=3,
+    )
+    current_player = models.CharField(max_length=1, default="X")
+    winner = models.CharField(max_length=1, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
